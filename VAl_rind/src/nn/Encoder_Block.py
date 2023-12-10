cff1990d69b42eb0395b23224aad9f2a19f20ba7
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-12-10 15:18:40 +0530

    Update Encoder_Block.py

--- VAl_rind/src/nn/Encoder_Block.py
+++ VAl_rind/src/nn/Encoder_Block.py
@@ -1,9 +1,19 @@
 import val_rind , mhsa , linear , Module
 
 class Encoder_Block(Module):
+    '''
+    Encoder Block class for the Transformer model. Inherits from the Module class
+
+    Parameters
+    
+            1) model_dim : Model dimension
+    '''
 
     def __init__(self , model_dim):
-
+            
+        '''
+        Constructor for the Encoder Block class
+        '''
         self.model_dim = model_dim
         
         self.mhsa = mhsa(self.model_dim)
@@ -12,6 +22,14 @@ class Encoder_Block(Module):
         self.parameters = [self.mhsa.parameters]
 
     def forward(self , inps):
+
+        '''
+        Forward pass for the Encoder Block class
+
+        Parameters
+            
+                1) inps : Inputs to the Encoder Block
+        '''
 
         attention , weights = self.mhsa.forward(inps , inps, inps)
 
